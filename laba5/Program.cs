420506fa7f6ec5a5d0a190640b6a446fffb68ba1
Dana <danarojik@mail.ru>
2023-04-25 11:13:48 +0300

    laba5

---
+++ laba5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        char op = char.Parse(Console.ReadLine());
+
+        double result = 0;
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Неверная операция");
+                return;
+        }
+
+        Console.WriteLine("Результат: " + result);
+    }
+}
+
